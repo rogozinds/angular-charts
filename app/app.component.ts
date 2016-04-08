a6e56dcd5ff277f68a82cdef249ce73acfdd25f5
Dmitrii Rogozin <dmitrii@vaadin.com>
2016-04-08 13:27:35 +0300

    Problem with databinding

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,29 +3,39 @@ import {VaadinCharts, DataSeries } from 'vaadin-charts';
 import {Directive, ViewContainerRef, ElementRef} from 'angular2/core';
 @Component({
     selector: 'my-app',
-    template: `<div>Some div</div>
+    template: `
+    <div>Data:</div>
+    <input (keyup)="onKey($event)">
     <vaadin-line-chart>
     <chart-title>
         Some title
     </chart-title>
     <subtitle>Some subtitle</subtitle>
-    <data-series>
-        <data>123,4214, 241, 421</data>
+    <data-series name="Data as tag">
+        <data>123,214, 241, 421</data>
     </data-series>
-    <!--<data-series name="Girls" data="123,3232,42">-->
-    <!--</data-series>-->
+    <data-series name="Data as attribute" data="[123,32,42,32]">
+    </data-series>
+    <data-series name="Data with data binding" [data]="dummyData">
+    </data-series>
+
     </vaadin-line-chart>`,
     directives: [VaadinCharts, DataSeries]
 })
 export class AppComponent {
-    dummyData = [123,421,42];
+    dummyData = [55,251,360];
     el:HTMLElement;
     onKey(event:any) {
-        //     this.dummyData=(<HTMLInputElement>event.target).value;
         this.dummyData=[];
         var self=this;
         setTimeout(function(){
-            self.dummyData=(<HTMLInputElement>event.target).value;
+            var tmpData=(<HTMLInputElement>event.target).value;
+            //try {
+            //    tmpData = JSON.parse("[" + tmpData + "]");
+            //} catch (err) {
+            //}
+
+            self.dummyData = tmpData;
         }, 0);
     }
 }
