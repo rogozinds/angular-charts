e7ff62c52c677806bf641b4ae1ad9d3cb01b3bb9
Dmitrii Rogozin <dmitrii@vaadin.com>
2016-04-08 11:37:17 +0300

    Simple chart with data, without data-binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,31 @@
 import {Component} from 'angular2/core';
-import { VaadinCharts, DataSeries } from 'vaadin-charts';
+import {VaadinCharts, DataSeries } from 'vaadin-charts';
+import {Directive, ViewContainerRef, ElementRef} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `<div>Some div</div>
-    <vaadin-area-chart>
+    <vaadin-line-chart>
     <chart-title>
         Some title
     </chart-title>
-    </vaadin-area-chart>`,
+    <subtitle>Some subtitle</subtitle>
+    <data-series>
+        <data>123,4214, 241, 421</data>
+    </data-series>
+    <!--<data-series name="Girls" data="123,3232,42">-->
+    <!--</data-series>-->
+    </vaadin-line-chart>`,
     directives: [VaadinCharts, DataSeries]
 })
-export class AppComponent { }
+export class AppComponent {
+    dummyData = [123,421,42];
+    el:HTMLElement;
+    onKey(event:any) {
+        //     this.dummyData=(<HTMLInputElement>event.target).value;
+        this.dummyData=[];
+        var self=this;
+        setTimeout(function(){
+            self.dummyData=(<HTMLInputElement>event.target).value;
+        }, 0);
+    }
+}
